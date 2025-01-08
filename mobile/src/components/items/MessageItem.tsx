495b84f115c0d9769df4caa8a68d3f49547036df
Ruslan Khilkovets <ruslanxilkovec@gmail.com>
2025-01-08 15:51:56 +0200

    added message actions

--- mobile/src/components/items/MessageItem.tsx
+++ mobile/src/components/items/MessageItem.tsx
@@ -1,14 +1,26 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Animated} from 'react-native';
-import {useTypedSelector} from '@/hooks';
-import {useTranslation} from 'react-i18next';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Animated,
+  TouchableWithoutFeedback,
+  Modal,
+  Pressable,
+} from 'react-native';
 import moment from 'moment';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 import i18n from 'i18next';
 import 'moment/locale/uk';
 import 'moment/locale/uk';
+import Clipboard from '@react-native-clipboard/clipboard';
+import {useTranslation} from 'react-i18next';
+
 import {useTheme} from '@/context/Theme/ThemeContext';
+import {useTypedSelector} from '@/hooks';
+
 
 interface IMessageItemProps {
   message: any;
@@ -26,6 +38,7 @@ const MessageItem = ({message}: IMessageItemProps) => {
   const [equalizerHeights, setEqualizerHeights] = useState<number[]>([
     7, 8, 15, 10, 5, 7, 10, 7, 8, 8, 15, 10, 15, 5, 15, 12,
   ]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const {t} = useTranslation();
   const {theme, colorScheme} = useTheme();
@@ -72,80 +85,119 @@ const MessageItem = ({message}: IMessageItemProps) => {
     }
   }, [isPlaying]);
 
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleLongPress = () => {
+    setModalVisible(true);
+  };
+
+  const onCopyMessageHandle = () => {
+    Clipboard.setString(message?.text);
+    handleCloseModal();
+  }
+
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          alignSelf: isMessageMine ? 'flex-end' : 'flex-start',
-          backgroundColor: theme[colorScheme].bgTertiary,
-        },
-      ]}>
-      {message?.audioPath ? (
-        <TouchableOpacity onPress={playAudio} style={styles.audioContainer}>
-          <Icon
-            name={isPlaying ? 'pause-circle-filled' : 'play-circle-filled'}
-            size={30}
-            color={theme[colorScheme].textPrimary}
-          />
-          <View style={styles.equalizer}>
-            {equalizerHeights.map((height, index) => (
-              <Animated.View
-                key={index}
-                style={[
-                  styles.equalizerBar,
-                  {height},
-                  {backgroundColor: theme[colorScheme].textPrimary},
-                ]}
-              />
-            ))}
-          </View>
-        </TouchableOpacity>
-      ) : (
-        <Text
-          style={[styles.messageText, {color: theme[colorScheme].textPrimary}]}>
-          {message?.text}
-        </Text>
-      )}
-
-      {message?.duration ? (
-        <Text style={styles.audioDuration}>
-          {`${moment.utc(currentPosition * 1000).format('mm:ss')} / ${moment
-            .utc(message?.duration * 1000)
-            .format('mm:ss')}`}
-        </Text>
-      ) : null}
-
+    <TouchableWithoutFeedback onPress={handleLongPress}>
       <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'flex-end',
-          gap: 10,
-          justifyContent: 'flex-end',
-        }}>
-        <Text style={[styles.date, {color: theme[colorScheme].textPrimary}]}>
-          {moment(message?.createdAt).locale(currentLanguage).calendar()}
-        </Text>
-
-        {isMessageMine && (
-          <View style={styles.statusContainer}>
-            {message?.isRead ? (
-              <Icon
-                name="done-all"
-                size={16}
-                color={theme[colorScheme].textPrimary}
-              />
-            ) : (
-              <Icon
-                name="done"
-                size={16}
-                color={theme[colorScheme].textPrimary}
-              />
-            )}
-          </View>
+        style={[
+          styles.container,
+          {
+            alignSelf: isMessageMine ? 'flex-end' : 'flex-start',
+            backgroundColor: theme[colorScheme].bgTertiary,
+          },
+        ]}>
+        {message?.audioPath ? (
+          <TouchableOpacity onPress={playAudio} style={styles.audioContainer}>
+            <Icon
+              name={isPlaying ? 'pause-circle-filled' : 'play-circle-filled'}
+              size={30}
+              color={theme[colorScheme].textPrimary}
+            />
+            <View style={styles.equalizer}>
+              {equalizerHeights.map((height, index) => (
+                <Animated.View
+                  key={index}
+                  style={[
+                    styles.equalizerBar,
+                    {height},
+                    {backgroundColor: theme[colorScheme].textPrimary},
+                  ]}
+                />
+              ))}
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <Text
+            style={[
+              styles.messageText,
+              {color: theme[colorScheme].textPrimary},
+            ]}>
+            {message?.text}
+          </Text>
         )}
+
+        {message?.duration ? (
+          <Text style={styles.audioDuration}>
+            {`${moment.utc(currentPosition * 1000).format('mm:ss')} / ${moment
+              .utc(message?.duration * 1000)
+              .format('mm:ss')}`}
+          </Text>
+        ) : null}
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'flex-end',
+            gap: 10,
+            justifyContent: 'flex-end',
+          }}>
+          <Text style={[styles.date, {color: theme[colorScheme].textPrimary}]}>
+            {moment(message?.createdAt).locale(currentLanguage).calendar()}
+          </Text>
+
+          {isMessageMine && (
+            <View style={styles.statusContainer}>
+              {message?.isRead ? (
+                <Icon
+                  name="done-all"
+                  size={16}
+                  color={theme[colorScheme].textPrimary}
+                />
+              ) : (
+                <Icon
+                  name="done"
+                  size={16}
+                  color={theme[colorScheme].textPrimary}
+                />
+              )}
+            </View>
+          )}
+        </View>
+        <Modal
+          transparent={true}
+          animationType="fade"
+          visible={modalVisible}
+          onRequestClose={handleCloseModal}>
+          <TouchableWithoutFeedback onPress={handleCloseModal}>
+            <View style={[styles.modalBackground, {backgroundColor: theme[colorScheme].shadow}]}>
+              <View style={[styles.modalContainer, {backgroundColor: theme[colorScheme].bgPrimary}]}>
+                <Pressable style={styles.modalOption}  android_ripple={{color: theme[colorScheme].bgSecondary, borderless: false}} onPress={onCopyMessageHandle}>
+                  <Text style={[styles.modalText,  {color: theme[colorScheme].textPrimary}]}>{t('actions.Copy')}</Text>
+                </Pressable>
+                <Pressable style={styles.modalOption}  android_ripple={{color: theme[colorScheme].bgSecondary, borderless: false}}>
+                  <Text style={[styles.modalText, {color: theme[colorScheme].textPrimary}]}>{t('actions.Edit')}</Text>
+                </Pressable>
+                <Pressable style={styles.modalOption}  android_ripple={{color: theme[colorScheme].bgSecondary, borderless: false}}>
+                  <Text style={[styles.modalText, {color: theme[colorScheme].textPrimary}]}>{t('actions.Delete')}</Text>
+                </Pressable>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -196,5 +248,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 5,
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    borderRadius: 10,
+    width: 200,
+  },
+  modalOption: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  modalText: {
+    fontSize: 20,
   },
 });
