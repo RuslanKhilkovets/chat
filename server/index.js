daff02f4b03f43b0e0989b0988c4fb7f654edd30
Ruslan_Khilkovets <ruslanxilkovec@gmail.com>
2025-03-20 12:45:18 +0200

    added chat socket

--- server/index.js
+++ server/index.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const { Server } = require('socket.io');
-const rateLimit = require('express-rate-limit');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { Server } = require("socket.io");
+const rateLimit = require("express-rate-limit");
 
-const userRoute = require('./routes/userRoute');
-const chatRoute = require('./routes/chatRoute');
-const adminRoute = require('./routes/adminRoute');
-const tokenRoute = require('./routes/tokenRoute');
-const messageRoute = require('./routes/messageRoute');
-const logRoute = require('./routes/logRoute');
-const mediaRoute = require('./routes/mediaRoute');
-const authMiddleware = require('./middleware/authMiddleware');
+const userRoute = require("./routes/userRoute");
+const chatRoute = require("./routes/chatRoute");
+const adminRoute = require("./routes/adminRoute");
+const tokenRoute = require("./routes/tokenRoute");
+const messageRoute = require("./routes/messageRoute");
+const logRoute = require("./routes/logRoute");
+const mediaRoute = require("./routes/mediaRoute");
+const authMiddleware = require("./middleware/authMiddleware");
 
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 100, // Limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later.',
+  message: "Too many requests from this IP, please try again later.",
 });
 
 app.use(limiter);
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/users', userRoute);
-app.use('/api/admin', authMiddleware, adminRoute);
-app.use('/api/chats', authMiddleware, chatRoute);
-app.use('/api/messages', authMiddleware, messageRoute);
-app.use('/api/token', authMiddleware, tokenRoute);
-app.use('/api/logs', logRoute);
-app.use('/api/media', authMiddleware, mediaRoute);
+app.use("/api/users", userRoute);
+app.use("/api/admin", authMiddleware, adminRoute);
+app.use("/api/chats", authMiddleware, chatRoute);
+app.use("/api/messages", authMiddleware, messageRoute);
+app.use("/api/token", authMiddleware, tokenRoute);
+app.use("/api/logs", logRoute);
+app.use("/api/media", authMiddleware, mediaRoute);
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
@@ -44,40 +44,40 @@ const expressServer = app.listen(port, (req, res) => {
 mongoose
   .connect(uri)
   .then(() => {
-    console.log('mongo connected');
+    console.log("mongo connected");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(`mongo error ${err.message}`);
   });
 
 const io = new Server(expressServer, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   },
 });
 
 let onlineUsers = new Map();
 
-io.on('connection', socket => {
-  console.log('New connection', socket.id);
+io.on("connection", (socket) => {
+  console.log("New connection", socket.id);
 
-  socket.emit('getOnlineUsers', Array.from(onlineUsers.values()));
+  socket.emit("getOnlineUsers", Array.from(onlineUsers.values()));
 
-  socket.on('addNewUser', userId => {
+  socket.on("addNewUser", (userId) => {
     if (!onlineUsers.has(userId) && userId !== null) {
       onlineUsers.set(userId, { userId, socketId: socket.id });
     }
 
-    io.emit('getOnlineUsers', Array.from(onlineUsers.values()));
+    io.emit("getOnlineUsers", Array.from(onlineUsers.values()));
   });
 
-  socket.on('sendMessage', message => {
+  socket.on("sendMessage", (message) => {
     const user = onlineUsers.get(message.recipientId);
 
     if (user) {
-      io.to(user.socketId).emit('getMessage', message);
-      io.to(user.socketId).emit('getNotification', {
+      io.to(user.socketId).emit("getMessage", message);
+      io.to(user.socketId).emit("getNotification", {
         senderId: message.senderId,
         isRead: false,
         date: new Date(),
@@ -85,91 +85,108 @@ io.on('connection', socket => {
     }
   });
 
-  socket.on('typingStart', ({ chatId, senderId, recipientId }) => {
+  socket.on("typingStart", ({ chatId, senderId, recipientId }) => {
     const recipient = onlineUsers.get(recipientId);
 
     if (recipient) {
-      io.to(recipient.socketId).emit('typingStart', { chatId, senderId });
+      io.to(recipient.socketId).emit("typingStart", { chatId, senderId });
     }
   });
 
-  socket.on('typingStop', ({ chatId, senderId, recipientId }) => {
+  socket.on("typingStop", ({ chatId, senderId, recipientId }) => {
     const recipient = onlineUsers.get(recipientId);
 
     if (recipient) {
-      io.to(recipient.socketId).emit('typingStop', { chatId, senderId });
+      io.to(recipient.socketId).emit("typingStop", { chatId, senderId });
     }
   });
 
-  socket.on('recordingStart', ({ chatId, senderId, recipientId }) => {
+  socket.on("recordingStart", ({ chatId, senderId, recipientId }) => {
     const recipient = onlineUsers.get(recipientId);
 
     if (recipient) {
-      io.to(recipient.socketId).emit('recordingStart', { chatId, senderId });
+      io.to(recipient.socketId).emit("recordingStart", { chatId, senderId });
     }
   });
 
-  socket.on('recordingStop', ({ chatId, senderId, recipientId }) => {
+  socket.on("recordingStop", ({ chatId, senderId, recipientId }) => {
     const recipient = onlineUsers.get(recipientId);
 
     if (recipient) {
-      io.to(recipient.socketId).emit('recordingStop', { chatId, senderId });
+      io.to(recipient.socketId).emit("recordingStop", { chatId, senderId });
     }
   });
-
-  socket.on('messageRead', async ({ chatId, messageIds, senderId, recipientId }) => {
-    messageIds.forEach(messageId => {
-      const sender = onlineUsers.get(senderId);
-
-      if (sender) {
-        io.to(sender.socketId).emit('messageRead', { chatId, messageId });
-      }
-
-      const recipient = onlineUsers.get(recipientId);
-
-      if (recipient) {
-        io.to(recipient.socketId).emit('messageRead', { chatId, messageId });
-      }
-    });
+  socket.on("chatCreated", ({ chatId, chatData }) => {
+    io.emit("chatCreated", chatData);
   });
 
-  socket.on('disconnect', () => {
+  socket.on(
+    "messageRead",
+    async ({ chatId, messageIds, senderId, recipientId }) => {
+      messageIds.forEach((messageId) => {
+        const sender = onlineUsers.get(senderId);
+
+        if (sender) {
+          io.to(sender.socketId).emit("messageRead", { chatId, messageId });
+        }
+
+        const recipient = onlineUsers.get(recipientId);
+
+        if (recipient) {
+          io.to(recipient.socketId).emit("messageRead", { chatId, messageId });
+        }
+      });
+    }
+  );
+
+  socket.on("disconnect", () => {
     for (let [userId, user] of onlineUsers) {
       if (user.socketId === socket.id) {
         onlineUsers.delete(userId);
         break;
       }
     }
-    console.log('User disconnected', socket.id);
+    console.log("User disconnected", socket.id);
 
-    io.emit('getOnlineUsers', Array.from(onlineUsers.values()));
+    io.emit("getOnlineUsers", Array.from(onlineUsers.values()));
   });
-  
-  socket.on('deleteMessage', ({ chatId, messageId, senderId, recipientId }) => {
+
+  socket.on("deleteMessage", ({ chatId, messageId, senderId, recipientId }) => {
     const sender = onlineUsers.get(senderId);
 
     if (sender) {
-      io.to(sender.socketId).emit('messageDeleted', { chatId, messageId });
+      io.to(sender.socketId).emit("messageDeleted", { chatId, messageId });
     }
 
     const recipient = onlineUsers.get(recipientId);
 
     if (recipient) {
-      io.to(recipient.socketId).emit('messageDeleted', { chatId, messageId });
+      io.to(recipient.socketId).emit("messageDeleted", { chatId, messageId });
     }
   });
 
-  socket.on('editMessage', ({ chatId, messageId, newContent, senderId, recipientId }) => {
-    const sender = onlineUsers.get(senderId);
+  socket.on(
+    "editMessage",
+    ({ chatId, messageId, newContent, senderId, recipientId }) => {
+      const sender = onlineUsers.get(senderId);
 
-    if (sender) {
-      io.to(sender.socketId).emit('messageEdited', { chatId, messageId, newContent });
+      if (sender) {
+        io.to(sender.socketId).emit("messageEdited", {
+          chatId,
+          messageId,
+          newContent,
+        });
+      }
+
+      const recipient = onlineUsers.get(recipientId);
+
+      if (recipient) {
+        io.to(recipient.socketId).emit("messageEdited", {
+          chatId,
+          messageId,
+          newContent,
+        });
+      }
     }
-
-    const recipient = onlineUsers.get(recipientId);
-
-    if (recipient) {
-      io.to(recipient.socketId).emit('messageEdited', { chatId, messageId, newContent });
-    }
-  });
+  );
 });
