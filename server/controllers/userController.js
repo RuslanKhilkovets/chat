637fac61a8cdee87a24efcabaaf0b24d27c97e1d
Ruslan_Khilkovets <ruslanxilkovec@gmail.com>
2025-04-18 11:59:32 +0300

    /

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,15 @@
-const userModel = require('../models/userModel');
-const tokenModel = require('../models/tokenModel');
-const bcrypt = require('bcrypt');
-const validator = require('validator');
-const jwt = require('jsonwebtoken');
-const logger = require('../utils/logger');
-const { default: mongoose } = require('mongoose');
+const userModel = require("../models/userModel");
+const tokenModel = require("../models/tokenModel");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const logger = require("../utils/logger");
+const { default: mongoose } = require("mongoose");
 
-const createToken = _id => {
+const createToken = (_id) => {
   const jwtkey = process.env.JWT_SECRET_KEY;
 
-  return jwt.sign({ _id }, jwtkey, { expiresIn: '3d' });
+  return jwt.sign({ _id }, jwtkey, { expiresIn: "30d" });
 };
 
 const registerUser = async (req, res) => {
@@ -19,23 +19,30 @@ const registerUser = async (req, res) => {
     let user = await userModel.findOne({ email });
 
     if (user) {
-      return res.status(400).json({ message: 'User with given email already exists' });
+      return res
+        .status(400)
+        .json({ message: "User with given email already exists" });
     }
 
     if (!name || !email || !password || !registerToken)
-      return res.status(400).json({ message: 'All fields must be provided' });
+      return res.status(400).json({ message: "All fields must be provided" });
 
     if (!validator.isEmail(email)) {
-      return res.status(400).json({ message: 'Invalid email' });
+      return res.status(400).json({ message: "Invalid email" });
     }
 
     if (!validator.isStrongPassword(password))
-      return res.status(400).json({ message: 'Password must be a strong' });
+      return res.status(400).json({ message: "Password must be a strong" });
 
-    const tokenDoc = await tokenModel.findOne({ token: registerToken, isUsed: false });
+    const tokenDoc = await tokenModel.findOne({
+      token: registerToken,
+      isUsed: false,
+    });
 
     if (!tokenDoc)
-      return res.status(400).json({ message: 'Invalid or already used registration token' });
+      return res
+        .status(400)
+        .json({ message: "Invalid or already used registration token" });
 
     user = new userModel({ name, email, password, registerToken, phone, tag });
 
@@ -49,12 +56,14 @@ const registerUser = async (req, res) => {
 
     const token = createToken(user._id);
 
-    logger.info(`User with email ${email} registered. Used token ${registerToken}`);
+    logger.info(
+      `User with email ${email} registered. Used token ${registerToken}`
+    );
 
     res.status(200).json({ _id: user._id, name, email, registerToken, token });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server Error', error: err });
+    res.status(500).json({ message: "Server Error", error: err });
   }
 };
 
@@ -63,24 +72,24 @@ const checkPassword = async (req, res) => {
 
   try {
     if (!id || !mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ message: 'Invalid or missing user ID' });
+      return res.status(400).json({ message: "Invalid or missing user ID" });
     }
 
     const user = await userModel.findById(id);
 
     if (!user) {
-      return res.status(400).json({ message: 'User not found' });
+      return res.status(400).json({ message: "User not found" });
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
     if (isPasswordCorrect) {
-      return res.status(200).json({ message: 'Password is correct' });
+      return res.status(200).json({ message: "Password is correct" });
     }
 
-    return res.status(400).json({ message: 'Incorrect password' });
+    return res.status(400).json({ message: "Incorrect password" });
   } catch (err) {
-    res.status(500).json({ message: 'Server Error', error: err.message });
+    res.status(500).json({ message: "Server Error", error: err.message });
   }
 };
 
@@ -90,11 +99,13 @@ const loginUser = async (req, res) => {
   try {
     let user = await userModel.findOne({ email });
 
-    if (!user) return res.status(400).json({ message: 'Invalid email or password' });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
 
     const isValidPassword = await bcrypt.compare(password, user.password);
 
-    if (!isValidPassword) return res.status(400).json({ message: 'Invalid password' });
+    if (!isValidPassword)
+      return res.status(400).json({ message: "Invalid password" });
 
     const token = createToken(user._id);
 
@@ -112,17 +123,17 @@ const findUser = async (req, res) => {
 
   try {
     if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
-      return res.status(400).json({ message: 'Invalid or missing user ID' });
+      return res.status(400).json({ message: "Invalid or missing user ID" });
     }
 
     const user = await userModel.findById(userId);
 
-    if (!user) return res.status(404).json({ message: 'User not found' });
+    if (!user) return res.status(404).json({ message: "User not found" });
 
     res.status(200).json(user);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server Error', error: err });
+    res.status(500).json({ message: "Server Error", error: err });
   }
 };
 
@@ -132,23 +143,24 @@ const findUsersByNameOrTag = async (req, res) => {
   try {
     if (!stringQuery)
       res.status(400).json({
-        message: 'Bad request. Query can not be empty and must be defined!',
+        message: "Bad request. Query can not be empty and must be defined!",
         users: [],
       });
 
     const users = await userModel.find({
       $or: [
-        { name: { $regex: stringQuery, $options: 'i' } },
-        { tag: { $regex: stringQuery, $options: 'i' } },
+        { name: { $regex: stringQuery, $options: "i" } },
+        { tag: { $regex: stringQuery, $options: "i" } },
       ],
     });
 
-    if (!users.length) return res.status(404).json({ message: 'User not found', users: [] });
+    if (!users.length)
+      return res.status(404).json({ message: "User not found", users: [] });
 
     res.status(200).json({ users });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server Error', error: err });
+    res.status(500).json({ message: "Server Error", error: err });
   }
 };
 
@@ -157,13 +169,13 @@ const getUser = async (req, res) => {
     const users = await userModel.find();
 
     if (users.length === 0) {
-      return res.status(404).json({ message: 'No users found' });
+      return res.status(404).json({ message: "No users found" });
     }
 
     res.status(200).json({ users });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server Error', error: err.message });
+    res.status(500).json({ message: "Server Error", error: err.message });
   }
 };
 
@@ -173,17 +185,17 @@ const updateUser = async (req, res) => {
 
   try {
     if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
-      return res.status(400).json({ message: 'Invalid or missing user ID' });
+      return res.status(400).json({ message: "Invalid or missing user ID" });
     }
 
     const user = await userModel.findById(userId);
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     if (email && !validator.isEmail(email)) {
-      return res.status(400).json({ message: 'Invalid email format' });
+      return res.status(400).json({ message: "Invalid email format" });
     }
 
     if (name !== undefined) user.name = name;
@@ -196,10 +208,12 @@ const updateUser = async (req, res) => {
 
     logger.info(`User with ID ${userId} updated successfully.`);
 
-    res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+    res
+      .status(200)
+      .json({ message: "User updated successfully", user: updatedUser });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server Error', error: err.message });
+    res.status(500).json({ message: "Server Error", error: err.message });
   }
 };
 
